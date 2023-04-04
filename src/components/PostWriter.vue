ddd01586b7fcc61619675e63341a41b22bc09f29
sam.brewer13@yahoo.com <samuel.brewer@improving.com>
2023-04-04 12:27:41 -0600

    implementing marked

--- src/components/PostWriter.vue
+++ src/components/PostWriter.vue
@@ -1,12 +1,38 @@
 <script lang="ts" setup>
-import { ref } from "vue";
+import { ref, onMounted, watch, watchEffect } from "vue"
 import { TimelinePost } from "../posts"
+import { marked } from "marked"
 
 const props = defineProps<{
     post: TimelinePost
 }>()
 
 const title = ref(props.post.title)
+const content = ref(props.post.markdown)
+const html = ref('')
+const contentEditable = ref<HTMLDivElement>()
+
+watch(content, (newContent) => {
+    marked.parse(newContent, (err, parseResult) => {
+    html.value = parseResult
+    })
+}, {
+    immediate: true
+})
+
+onMounted(() => {
+    if (!contentEditable.value) {
+        throw Error('ContentEditable DOM node was not found')
+    }
+    contentEditable.value.innerText = content.value
+})
+
+function handleInput () {
+    if (!contentEditable.value) {
+        throw Error('ContentEditable DOM node was not found')
+    }
+    content.value = contentEditable.value.innerText
+}
 </script>
 
 <template>
@@ -15,8 +41,17 @@ const title = ref(props.post.title)
             <div class="field">
                 <div class="label">Post title</div>
                 <input type="text" class="input" v-model="title">
-                {{ title }}
             </div>
+            <div contenteditable ref="contentEditable" />
+        </div>
+    </div>
+
+    <div class="columns">
+        <div class="column">
+            <div contenteditable ref="contentEditable" @input="handleInput" />
+        </div>
+        <div class="column">
+            <div v-html="html" />
         </div>
     </div>
 </template>

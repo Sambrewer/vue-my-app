ebefc959deb04f58e80acafce9f0ceabd90508a9
sam.brewer13@yahoo.com <samuel.brewer@improving.com>
2023-04-04 10:41:54 -0600

    setting up new post page route

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
+<script setup lang="ts">
+import Navbar from "./components/Navbar.vue"
+</script>
+
 <template>
   <div class="section">
 
     <div class="container">
+      <Navbar />
       <RouterView />
     </div>
 
